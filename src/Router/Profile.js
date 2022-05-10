03bb87ba102021f4dd92e0b79f6e43dfc27cf7d1
sarthak vaidya <sarthakvaidya0@gmail.com>
2022-05-10 09:31:44 +0530

    SV: added code for the axios and fetch api

--- src/Router/Profile.js
+++ src/Router/Profile.js
@@ -1,10 +1,115 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 
+import axios from "axios";
+
 function Profile(props) {
+  // const Api = axios.create({
+  //   baseURL: "https://jsonplaceholder.typicode.com/posts",
+  //   headers: {
+  //     Authorization: "Bearer dnfksjdfksjhf", //the token is a variable which holds the token
+  //   },
+  // });
   let { elemtId } = useParams();
-  //console.log("router params", params);
-  return <div>Profile {elemtId}</div>;
+
+  const [posts, setPosts] = React.useState([]);
+
+  React.useEffect(() => {
+    // fetch(`https://jsonplaceholder.typicode.com/posts`)
+    //   .then((result) => {
+    //     console.log("success");
+    //     return result.json();
+    //   })
+    //   .then((data) => {
+    //     console.log(data);
+    //     setPosts(data);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+    // fetch("https://jsonplaceholder.typicode.com/posts", {
+    //   method: "POST",
+    //   body: JSON.stringify({
+    //     title: "foo",
+    //     body: "bar",
+    //     userId: 1,
+    //   }),
+    //   headers: {
+    //     "Content-type": "application/json; charset=UTF-8",
+    //   },
+    // })
+    //   .then((response) => response.json())
+    //   .then((json) => console.log(json));
+
+    //axios get method
+    // axios
+    //   .get("https://jsonplaceholder.typicode.com/posts")
+    //   .then((result) => {
+    //     console.log(result.data);
+    //     setPosts(result.data);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
+    //axios post method
+
+    const headers = {
+      "Content-Type": "application/json",
+      Authorization: "JWT fefege...",
+    };
+
+    let data = {
+      title: "foo",
+      body: "bar",
+      userId: 1,
+    };
+
+    // axios
+    //   .post("https://jsonplaceholder.typicode.com/posts", data, {
+    //     headers: headers,
+    //   })
+    //   .then((result) => {
+    //     console.log(result.data);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
+    // const getData = async () => {
+    //   try {
+    //     const result = await axios.get(
+    //       "https://jsonplaceholder.typicode.com/psts"
+    //     );
+    //     console.log(result.data);
+    //   } catch (error) {
+    //     console.log(error);
+    //   }
+    // };
+    // getData();
+
+    const getData = async () => {
+      try {
+        const result = await fetch(
+          "https://jsonplaceholder.typicode.com/posts"
+        );
+
+        let response = await result.json();
+        console.log(response);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    getData();
+  }, []);
+
+  let list = posts.map((item) => <li key={item.id}>{item.title}</li>);
+  return (
+    <>
+      <h1>Profile {elemtId}</h1>
+      <ul>{list}</ul>
+    </>
+  );
 }
 
 export default Profile;
--- src/Router/Root.js
+++ src/Router/Root.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import AboutUs from "./AboutUs";
 import ContactUs from "./ContactUs";
+import Footer from "./Footer";
 import Home from "./Home";
 import Navigation from "./Navigation";
 import NotFound from "./NotFound";
@@ -13,14 +14,18 @@ function Root() {
     <>
       <BrowserRouter>
         <Navigation />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/aboutus" element={<AboutUs />} />
-          <Route path="/contactus" element={<ContactUs />} />
-          <Route path="/products" element={<Products />} />
-          <Route path={"/profile/:id"} element={<Profile />} />
-          <Route path="*" element={<NotFound />} />
-        </Routes>
+        <div style={{ minHeight: "60vh" }}>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/aboutus" element={<AboutUs />} />
+            <Route path="/contactus" element={<ContactUs />} />
+            <Route path="/products" element={<Products />} />
+
+            <Route path={"/profile/:id"} element={<Profile />}></Route>
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+        <Footer />
       </BrowserRouter>
     </>
   );
